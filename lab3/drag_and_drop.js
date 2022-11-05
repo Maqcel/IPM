84b3f8bd917dd42e56be7edd90babc6c8e84a76a
Maqcel <marcel.kozien@gmail.com>
2022-11-05 18:39:02 +0100

    handle drag

--- lab3/drag_and_drop.js
+++ lab3/drag_and_drop.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+
     function handleDragStart(e) {
-        console.log('test');
         this.style.opacity = '0.4';
     }
 
@@ -8,9 +8,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
         this.style.opacity = '1';
     }
 
+    function handleDragOver(e) {
+        if (e.preventDefault) {
+            e.preventDefault();
+        }
+
+        return false;
+    }
+
+    function handleDragEnter(e) {
+        this.classList.add('over');
+    }
+
+    function handleDragLeave(e) {
+        this.classList.remove('over');
+    }
+
     let items = document.querySelectorAll('.container .box');
     items.forEach(function (item) {
         item.addEventListener('dragstart', handleDragStart);
         item.addEventListener('dragend', handleDragEnd);
+        item.addEventListener('dragover', handleDragOver);
+        item.addEventListener('dragenter', handleDragEnter);
+        item.addEventListener('dragleave', handleDragLeave);
     });
 });
