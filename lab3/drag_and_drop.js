f507b17a3a995f1f72f7de04a5aa38ec70a9c420
Maqcel <marcel.kozien@gmail.com>
2022-11-05 18:27:29 +0100

    drag controls

---
+++ lab3/drag_and_drop.js
@@ -0,0 +1,13 @@
+function handleDragStart(e) {
+    this.style.opacity = '0.4';
+}
+
+function handleDragEnd(e) {
+    this.style.opacity = '1';
+}
+
+let items = document.querySelectorAll('.container .box');
+items.forEach(function (item) {
+    item.addEventListener('dragstart', handleDragStart);
+    item.addEventListener('dragend', handleDragEnd);
+});
