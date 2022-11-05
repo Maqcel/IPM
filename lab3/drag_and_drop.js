eac0270c4406da7608ebaa2a7be79d4e16e3c566
Maqcel <marcel.kozien@gmail.com>
2022-11-05 18:32:48 +0100

    loading dom

--- lab3/drag_and_drop.js
+++ lab3/drag_and_drop.js
@@ -1,13 +1,16 @@
-function handleDragStart(e) {
-    this.style.opacity = '0.4';
-}
+document.addEventListener('DOMContentLoaded', (event) => {
+    function handleDragStart(e) {
+        console.log('test');
+        this.style.opacity = '0.4';
+    }
 
-function handleDragEnd(e) {
-    this.style.opacity = '1';
-}
+    function handleDragEnd(e) {
+        this.style.opacity = '1';
+    }
 
-let items = document.querySelectorAll('.container .box');
-items.forEach(function (item) {
-    item.addEventListener('dragstart', handleDragStart);
-    item.addEventListener('dragend', handleDragEnd);
+    let items = document.querySelectorAll('.container .box');
+    items.forEach(function (item) {
+        item.addEventListener('dragstart', handleDragStart);
+        item.addEventListener('dragend', handleDragEnd);
+    });
 });
