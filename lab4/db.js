35e2ad334c16fd722bff6a621fac50c6b7608a06
Maqcel <marcel.kozien@gmail.com>
2022-11-19 18:40:26 +0100

    adding client to db

--- lab4/db.js
+++ lab4/db.js
@@ -9,8 +9,28 @@ dbReq.onupgradeneeded = function (event) {
 dbReq.onsuccess = function (event) {
     console.log('Creation success')
     db = event.target.result;
+
+    addCustomer(db, 'Marcin', 'Testowy', 17)
 }
 dbReq.onerror = function (event) {
     console.log('Creation error')
     alert('error opening database ' + event.target.errorCode);
+}
+
+function addCustomer(db, firstName, lastName, age) {
+    let transaction = db.transaction(['clients'], 'readwrite');
+    let store = transaction.objectStore('clients');
+
+    let customer = { firstName: firstName, lastName: lastName, age: age };
+    store.add(customer);
+
+    // Success
+    transaction.oncomplete = function () {
+        console.log('Customer: ', firstName, ' ', lastName, ' aged: ', age, ' added\n')
+    }
+
+    // Failed
+    transaction.onerror = function (event) {
+        alert('Error adding customer: ' + event.target.errorCode);
+    }
 }
