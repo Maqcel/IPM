252a0ee96d5c0d2157db3a109802823d2f0135b2
Maqcel <marcel.kozien@gmail.com>
2022-11-19 18:28:24 +0100

    creation of db

--- lab4/db.js
+++ lab4/db.js
@@ -1,2 +1,16 @@
 let db;
 let dbReq = indexedDB.open('myDatabase', 2);
+
+dbReq.onupgradeneeded = function (event) {
+    db = event.target.result;
+
+    let clients = db.createObjectStore('clients', { autoIncrement: true });
+}
+dbReq.onsuccess = function (event) {
+    console.log('Creation success')
+    db = event.target.result;
+}
+dbReq.onerror = function (event) {
+    console.log('Creation error')
+    alert('error opening database ' + event.target.errorCode);
+}
